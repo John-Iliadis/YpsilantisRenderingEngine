084737885354319432b7eede783b0c7810ab37e4
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-01-01 08:30:17 +1100

    initial commit

---
+++ src/pch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gianni on 1/01/2025.
+//
+
+#include "pch.hpp"
---
+++ src/pch.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Gianni on 1/01/2025.
+//
+
+#include <vector>
+#include <array>
+#include <iostream>
+#include <format>
+#include <source_location>
