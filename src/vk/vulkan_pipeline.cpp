95dab653ef43e04042e40d8332e2b54e44b6d8fc
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-02-05 09:26:22 +1100

    update vulkan shader

--- src/vk/vulkan_pipeline.cpp
+++ src/vk/vulkan_pipeline.cpp
@@ -4,6 +4,46 @@
 
 #include "vulkan_pipeline.hpp"
 
+VulkanShaderModule::VulkanShaderModule(const VulkanRenderDevice& renderDevice, const std::string& path)
+    : mRenderDevice(renderDevice)
+    , mShaderModule()
+{
+    std::ifstream shaderFile(path, std::ios::binary | std::ios::ate);
+
+    check(shaderFile.is_open(), std::format("Failed to open {}", path).c_str());
+
+    std::streamsize bufferSize = shaderFile.tellg();
+    std::vector<uint32_t> shaderSrc(bufferSize / 4);
+
+    shaderFile.seekg(0);
+    shaderFile.read(reinterpret_cast<char*>(shaderSrc.data()), bufferSize);
+
+    VkShaderModuleCreateInfo shaderModuleCreateInfo {
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .codeSize = 4 * shaderSrc.size(),
+        .pCode = shaderSrc.data()
+    };
+
+    VkResult result = vkCreateShaderModule(mRenderDevice.device, &shaderModuleCreateInfo, nullptr, &mShaderModule);
+    vulkanCheck(result, "Failed to create shader module.");
+
+    setDebugVulkanObjectName(mRenderDevice,
+                             VK_OBJECT_TYPE_SHADER_MODULE,
+                             path,
+                             mShaderModule);
+}
+
+VulkanShaderModule::~VulkanShaderModule()
+{
+    vkDestroyShaderModule(mRenderDevice.device, mShaderModule, nullptr);
+}
+
+VulkanShaderModule::operator VkShaderModule() const
+{
+    return mShaderModule;
+}
+
+
 VkPipelineShaderStageCreateInfo shaderStageCreateInfo(VkShaderStageFlagBits stage, VkShaderModule module)
 {
     return {
--- src/vk/vulkan_pipeline.hpp
+++ src/vk/vulkan_pipeline.hpp
@@ -5,6 +5,23 @@
 #ifndef VULKANRENDERINGENGINE_VULKAN_PIPELINE_HPP
 #define VULKANRENDERINGENGINE_VULKAN_PIPELINE_HPP
 
+#include "vulkan_render_device.hpp"
+#include "vulkan_utils.hpp"
+#include "../utils/utils.hpp"
+
+class VulkanShaderModule
+{
+public:
+    VulkanShaderModule(const VulkanRenderDevice& renderDevice, const std::string& path);
+    ~VulkanShaderModule();
+
+    operator VkShaderModule() const;
+
+private:
+    const VulkanRenderDevice& mRenderDevice;
+    VkShaderModule mShaderModule;
+};
+
 VkPipelineShaderStageCreateInfo shaderStageCreateInfo(VkShaderStageFlagBits stage, VkShaderModule module);
 
 #endif //VULKANRENDERINGENGINE_VULKAN_PIPELINE_HPP
